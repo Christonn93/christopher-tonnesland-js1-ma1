89e00a5c50028b719f776dacb7e2ca35da3bd8a8
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-01-26 17:52:19 +0100

    adding tasks
    Uploading tasks.
    Have been asking in Kevin Powell's discord for some help.
    
    Got some really good tips on how to use MDN, how to navigate it

--- js/script.js
+++ js/script.js
@@ -1,13 +1,87 @@
-const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
+// Question 1
+
+let cat = {
+    complain: function () {
+        console.log("Meow!");
     }
+};
+
+// Question 2
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.className = "subheading";
+
+/* 
+Reference used to solve this Question: 
+https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+*/
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+paragraphs.forEach((para) => {
+    para.style.color = "red";
+});
+
+
+// Question 6
+
+const resultsContainer = document.querySelector(".results");
+resultsContainer.style.backgroundColor = "yellow";
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+
+
+// Question 7
+
+const cats = [{
+    name: "Blob",
+    age: 10
+},
+{
+    name: "Harold",
+},
+{
+    name: "Blurt",
+    age: 21
+}
 ];
+
+function listElement(list){
+    for(var i = 0; i < cats.length; i++){
+        let catName = cats[i].name;
+        console.log(catName);
+    }
+}
+
+listElement();
+
+
+// Question 8
+
+/* const catAge = "Age unknown";
+const divContainer = document.createElement("div");
+
+function createCats(cats){
+
+        for(var i = 0; i < list.length; i++){
+
+            containerHeader = cats[i].name;
+            var containerHeader = document.createElement("h5");
+            textItem = cats[i].age;
+            var textItem = document.createElement("p");
+    }
+
+    cats.forEach((arrayList) => {
+    })
+
+}
+
+createCats(); */
---
+++ lessons/lessonjs.js
@@ -0,0 +1,28 @@
+// Question 1
+
+let cat = {
+    complain: function(){
+        console.log("Meow!");
+    }
+};
+
+// Question 2
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading"; 
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.Element.style = "subheading";
+
+// Question 5
+
+// Question 6
+
+// Question 7
+
+// Question 8
