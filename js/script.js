c1a6a27c0f48bfaefff653d36605fc3020a82665
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-01-28 09:09:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Question 1
 
 let cat = {
-    complain: function () {
-        console.log("Meow!");
-    }
+  complain: function () {
+    console.log("Meow!");
+  },
 };
 
 // Question 2
@@ -28,9 +28,8 @@ https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 
 const paragraphs = document.querySelectorAll("p");
 paragraphs.forEach((para) => {
-    para.style.color = "red";
+  para.style.color = "red";
 });
-
 
 // Question 6
 
@@ -38,75 +37,52 @@ const resultsContainer = document.querySelector(".results");
 resultsContainer.style.backgroundColor = "yellow";
 resultsContainer.innerHTML = "<p>New paragraph</p>";
 
-
 // Question 7
 
-const cats = [{
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
 
 function listElement(list) {
-    for (var i = 0; i < cats.length; i++) {
-        let list = cats[i].name;
-        console.log(list);
-    }
-};
+  for (var i = 0; i < cats.length; i++) {
+    let names = cats[i].name;
+    console.log(names);
+  }
+}
 
 listElement();
 
-
 // Question 8
-
-/* function createCats(cats) {
-
-    for (var i = 0; i < cats.length; i++) {
-        const name = document.createElement("h5").innerHTML = cats.name;
-        const age = document.createElement("p").innerHTML = cats.age;
-    }
-    
-    if (!cats[i].age) {
-        missingAge = "Age unknown";
-    } else {
-        const divContainer = document.querySelector(".cat-container").innerHTML = name + " " + age;
-        divContainer;
-    }
-}
-
-createCats(); */
-
 function createCats(cats) {
-    for (let q = 0; q < cats.length; q++) {
+  for (let q = 0; q < cats.length; q++) {
+    let catName = cats[q].name;
+    let catAge = cats[q].age;
+    const name = (document.createElement("h5").innerHTML = catName);
+    const age = (document.createElement("p").innerHTML = catAge);
+    const missingAge = "Age unknown";
 
-        let catName = cats[q].name;
-        let catAge = cats[q].age;
-        const name = document.createElement("h5").innerHTML = catName;
-        const age = document.createElement("p").innerHTML = catAge;
-        const missingAge = "Age unknown";
-        
-        if (!catAge) {
-            let divContainer = document.querySelector(".cat-container").innerHTML += `
+    if (!catAge) {
+      let divContainer = (document.querySelector(".cat-container").innerHTML += `
             <h5>${name}</h5>
             <p>${missingAge}</p>
-            `
-        } else {
-            let divContainer = document.querySelector(".cat-container").innerHTML += `
+            `);
+    } else {
+      let divContainer = (document.querySelector(".cat-container").innerHTML += `
             <h5>${name}</h5>
             <p>${age}</p>
-            `
-        }
+            `);
     }
-
-
-
-};
+  }
+}
 
 createCats(cats);
